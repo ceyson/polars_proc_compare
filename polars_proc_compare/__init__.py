3769117a13553ec7e7ce92b1fd85d026314be3ae
Jason Lee <cyeson@protonmail.com>
2025-11-14 21:25:21 -0500

    Fix package structure and imports
    - Update pyproject.toml to properly include all package files
    - Fix imports in __init__.py
    - Add batch_utils to package exports
    - Use absolute imports

--- polars_proc_compare/__init__.py
+++ polars_proc_compare/__init__.py
@@ -1,9 +1,17 @@
 """Polars DataFrame comparison utilities."""
 
-from .comparison_engine import DataCompare
-from .results import ComparisonResults
-from .memory_optimizer import MemoryOptimizedSchema
-from .disk_compare import DiskDataCompare
+from polars_proc_compare.comparison_engine import DataCompare
+from polars_proc_compare.disk_compare import DiskDataCompare
+from polars_proc_compare.memory_optimizer import MemoryOptimizedSchema
+from polars_proc_compare.results import ComparisonResults
+from polars_proc_compare.batch_utils import compare_in_batches, get_memory_usage
 
 __version__ = "0.1.0"
-__all__ = ['DataCompare', 'ComparisonResults', 'MemoryOptimizedSchema', 'DiskDataCompare']
+__all__ = [
+    "DataCompare",
+    "ComparisonResults",
+    "MemoryOptimizedSchema",
+    "DiskDataCompare",
+    "compare_in_batches",
+    "get_memory_usage",
+]
